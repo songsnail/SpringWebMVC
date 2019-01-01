c36e40332c2301ff9ff4eab5e226cca3cd5d4e66
songsnail <songsnail71@gmail.com>
2019-01-01 20:32:39 +0900

    10장 도서 쇼핑몰 : 도서 검색 오류의 예외 처리하기

---
+++ chap10/src/main/java/com/springmvc/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String welcome(Model model) {
+
+		model.addAttribute("greeting", "웹 쇼핑몰에 오신 것을 환영합니다!");
+		model.addAttribute("strapline", "Welcome to Web Market!");
+
+		return "forward:/welcome/greeting";
+	}
+
+	@RequestMapping("/welcome/greeting")
+	public String greeting() {
+		return "home";
+	}
+
+}
